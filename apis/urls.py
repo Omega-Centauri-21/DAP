b68e0b650bafd087be93389b5d8d1b7d2b0e098f
Omega-Centauri-21 <69707370+Omega-Centauri-21@users.noreply.github.com>
2022-07-25 12:39:34 +0530

    Update urls.py

--- apis/urls.py
+++ apis/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('details/', api_view.DetailView.as_view()),
-    path('attendance/', api_view.AttendanceView.as_view()),
-    path('marks/', api_view.MarksView.as_view()),
-    path('timetable/', api_view.TimetableView.as_view()),
+    path('patients/', api_view.DetailView.as_view()),
+    path('hospitals/', api_view.AttendanceView.as_view()),
+    path('medical_history/', api_view.MarksView.as_view()),
+    path('current_status/', api_view.TimetableView.as_view()),
 ]
